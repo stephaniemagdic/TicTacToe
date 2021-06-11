cfaca8b324b9ec581ffdc7bb17e95d6f52141238
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-06-10 20:35:50 -0600

    Add winning arrays to data.js file and add script tags to index.html

---
+++ src/data.js
@@ -0,0 +1,10 @@
+winningBoardSets = {
+  win1: ["TL", "TC", "TR"],
+  win2: ["ML", "MC", "MR"],
+  win3: ["BL", "BC", "BR"],
+  win4: ["TL", "ML", "BL"],
+  win5: ["TC", "MC", "BC"],
+  win6: ["TR", "MR", "BR"],
+  win7: ["TL", "MC", "BR"],
+  win8: ["TR", "MC", "BL"]
+}
