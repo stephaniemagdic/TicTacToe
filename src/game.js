933e4113f5c0bb0785778d8426aed22f2dd0ad89
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-06-11 15:11:20 -0600

    Add updateTurn and Reset methods on Game class

--- src/game.js
+++ src/game.js
@@ -1,31 +1,46 @@
 class Game {
   constructor() {
-    this.currentPlayers = [];
-    this.currentTurnPlayerIndex: 0;
+    this.players = [];
+    this.currentTurnIndexPosition: 0;
     this.totalTurnsTaken = 0;
   }
 
   //is this function below necessary? //not for local storage purpose
   //but for SRP purpose
   addPlayer(player) {
-    this.currentPlayers.push(player);
+    this.players.push(player);
   }
 
-  setUpGame() {
+  setUp() {
     var player1 = new Player(1, "star", 0);
 		var player2 = new Player(2, "heart", 0);
     this.addPlayer(player1);
     this.addPlayer(player2);
-    this.currentTurn = 0;
-    //add to method or Main.js?
-      //if (localStorage), call retrieveWins on currentPlayers array.
+    this.currentTurnIndexPosition = 0;
+      //if (localStorage), call retrieveWins on currentPlayers array---add to this method or Main.js?
   }
 
   addPlayerPosition(position){
-    this.currentPlayers[currentTurnPlayerIndex].takenPositions.push(position);
+    this.players[currentTurnIndexPosition].takenPositions.push(position);
   }
 
   updateTurn() {
+    if (this.currentTurnIndexPosition === 0) {
+      this.currentTurn === 1;
+    } else if (this.currentTurnIndexPosition === 1) {
+      this.currentTurn === 0;
+    }
 
+    this.totalTurnsTaken += 1;
   }
+
+  reset() {
+    for (var i = 0; i < this.players.length; i ++) {
+      this.players[i].takenPositions = [];
+    }
+    this.totalTurnsTaken = 0;
+    //could add extra logic... if you are the winner, you start. ie update currentTurnIndexPosition
+    //with winner.
+  }
+
 }
